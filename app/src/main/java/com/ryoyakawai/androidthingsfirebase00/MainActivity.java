449f34ef5654c93b6f22536150189b202b866b42
Ryoya KAWAI <ryoya.kawai@gmail.com>
2017-05-23 11:01:10 -0700

    Update MainActivity.java

--- app/src/main/java/com/ryoyakawai/androidthingsfirebase00/MainActivity.java
+++ app/src/main/java/com/ryoyakawai/androidthingsfirebase00/MainActivity.java
@@ -19,25 +19,6 @@ import java.io.IOException;
 
 import javax.xml.validation.Schema;
 
-/**
- * Skeleton of an Android Things activity.
- * <p>
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- * <p>
- * <pre>{@code
- * PeripheralManagerService service = new PeripheralManagerService();
- * mLedGpio = service.openGpio("BCM6");
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
- * mLedGpio.setValue(true);
- * }</pre>
- * <p>
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
- */
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
